9c3825196bc5b871a39397c031bbd5a0cfa0bab9
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-07-23 10:54:38 +0300

    fix Main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,36 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        MyList<String> list = new MyList();
+        MyList<String> list = new MyList<>();
         list.add(0,"a");
         list.add(1,"b");
-        list.add(2,"c");
-        list.add(3,"d");
         list.add("e");
         list.add(1,"f");
-        System.out.println(list);
-        //list.clear();
-
         list.set(0,"g");
         list.delete("d");
         System.out.println(list);
         System.out.println(list.get(0));
         System.out.println(list.contain("f"));
 
+        MyList<Dummy> dummyObjects = new MyList<>();
+        dummyObjects.add(new Dummy("test1"));
+        dummyObjects.add(new Dummy("test2"));
+        dummyObjects.add(0, new Dummy("test3"));
 
+        System.out.println(dummyObjects.get(0).value);
+
+
+    }
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class Dummy{
+        private String value;
     }
 }
